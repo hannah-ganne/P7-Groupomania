18f645b86027d17d8abb082e4d36a4ca768b26f5
hannahganne <ganne.hannah@gmail.com>
2022-07-03 21:38:12 +0200

    add profile page

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -1,16 +1,26 @@
 import user from '../assets/user.png'
+import '../utils/style/profile.css'
 
 export default function Profile() {
     return (
-        <section>
-            <h2>Profile</h2>
+        <section className="profile">
+            <h2 className="profile-title">Profile</h2>
             <div className="profile-info">
                 <img src={user} alt="user avatar"></img>
-                <p><span className="bold">Name: </span>Hannah Ganne</p>
-                <p><span className="bold">Department: </span>IT</p>
+                <div>
+                    <p><span className="bold">Name: </span>Hannah Ganne</p>
+                    <p><span className="bold">Department: </span>IT</p>
+                </div>
             </div>
             <div className="profile-detail">
-                <h3>At work I'm expert in...</h3>
+                <span className="bold uppercase">At work I'm expert in...</span>
+                <p>Workflow management, Frontend development</p>
+                <span className="bold uppercase">Personally I'm interested in...</span>
+                <p>Parenting advice, yoga</p>
+                <span className="bold uppercase">Describe yourself in one word</span>
+                <p>I am a <span className='bold uppercase'>dedicated</span> person</p>
+                <span className="bold uppercase">I am up for...</span>
+                <p>Coffee chat, Zoom meeting, Afterwork happy hour, Office parties, Collaborations</p>
                 
             </div>
         </section>
