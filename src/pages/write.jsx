ac40a1db24682b423fb5fc0953e865792eb0d70c
lanomade <ganne.hannah@gmail.com>
2022-07-15 22:25:03 +0200

    fetch dynamically topic list

--- src/pages/write.jsx
+++ src/pages/write.jsx
@@ -1,29 +1,21 @@
 import Button from '../components/Button'
 import '../utils/style/write.css'
+import { topics } from '../docs/list'
+import { useState } from 'react'
 
 export default function Write() {
+
+    const topicEl = topics.map(topic => {
+        return <option key={topic.id} value={topic.label}>{topic.label}</option>
+    })
+
     return (
         <section className="write">
             <form>
                 <input type="text" placeholder="Title" required />
                 <select required>
                     <option value="">Select topic</option>
-                    <option value="News">News</option>
-                    <option value="Team building">Team building</option>
-                    <option value="Workflow">Workflow</option>
-                    <option value="Productivity">Productivity</option>
-                    <option value="Leadership">Leadership</option>
-                    <option value="Communication">Communication</option>
-                    <option value="Career">Career</option>
-                    <option value="Technology">Technology</option>
-                    <option value="Networking">Networking</option>
-                    <option value="Just for laughs">Just for laughs</option>
-                    <option value="Suggestions">Suggestions</option>
-                    <option value="Insight">Insight</option>
-                    <option value="Industry">Industry</option>
-                    <option value="Events">Events</option>
-                    <option value="Learning and development">Learning and development</option>
-                    <option value="Other topics">Other topics</option>
+                    {topicEl}
                 </select>
                 <textarea rows="30" cols="100">Start writing about the topic you chose.
                     Please be respectful of your co-workers and use respectful language.
