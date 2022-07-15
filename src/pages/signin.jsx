ad3a3980d47e5add6b970c7fb0844a1e2efa4958
lanomade <ganne.hannah@gmail.com>
2022-07-15 22:24:07 +0200

    put in place signin logic

--- src/pages/signin.jsx
+++ src/pages/signin.jsx
@@ -1,9 +1,48 @@
 import logo from '../assets/icon-left-font-monochrome-black.svg'
 import Button from '../components/Button'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import '../utils/style/signup.css'
 
 export default function Signin() {
+    const [signinData, setSigninData] = useState({
+        "email": "",
+        "password": ""
+    })
+
+    function handleChange(event) {
+        setSigninData({ ...signinData, [event.target.name]: event.target.value })
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+
+        const signinOptions = {
+            method: "POST",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "email": signinData.email,
+                "password": signinData.password
+            })
+        }
+
+        fetch("http://localhost:3000/api/auth/login", signinOptions)
+            .then(res => {
+            if (res.ok) {
+                return res.json();
+            }
+                throw new Error("There's an error sending the data")
+            })
+            .then(data => {
+                sessionStorage.setItem("token", JSON.stringify(data.token))
+                document.location.href = './';
+            })
+            .catch(err => console.log(err))
+    }
+
     return (
         <>
             <header className="signup-header">
@@ -15,10 +54,10 @@ export default function Signin() {
             <main className="signup-main">
                 <div className="signup-form-container">
                     <h2>Sign in</h2>
-                    <form>
-                        <input placeholder='Email Address'/>
-                        <input placeholder='Password'/>
-                        <Button className="btn red" name="Sign in"/>
+                    <form onSubmit={handleSubmit}>
+                        <input placeholder='Email Address' name='email' value={signinData.email} onChange={handleChange}/>
+                        <input type='password' placeholder='Password' name='password' value={signinData.password} onChange={handleChange} />
+                        <Button className="btn red" name="Sign in" type="submit"/>
                         <p>You don't have an account? <Link to="/signup" className="bold">Sign up!</Link></p>
                     </form>
                 </div>
