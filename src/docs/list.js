3137b33efcfd6f6027d360f4942bfcc48b7a9d3a
lanomade <ganne.hannah@gmail.com>
2022-07-15 22:21:40 +0200

    add static list arrays

---
+++ src/docs/list.js
@@ -0,0 +1,196 @@
+const departments = [
+    {
+        id: 1,
+        label: 'Finance'
+    },
+    {
+        id: 2,
+        label: 'HR'
+    },
+    {
+        id: 3,
+        label: 'IT'
+    },
+    {
+        id: 4,
+        label: 'Marketing'
+    },
+    {
+        id: 5,
+        label: 'Operations'
+    },
+    {
+        id: 6,
+        label: 'Purchase'
+    },
+    {
+        id: 7,
+        label: 'Sales'
+    }
+]
+
+const topics = [
+
+    {
+        id:1,
+        label: "Team Building"
+    },
+    {
+        id:2,
+        label: "News"
+    },
+    {
+        id:3,
+        label: "Career"
+    },
+    {
+        id:5,
+        label: "Productivity"
+    },
+    {
+        id:6,
+        label: "Leadership"
+    },
+    {
+        id:7,
+        label: "Communication"
+    },
+    {
+        id:8,
+        label: "Technology"
+    },
+    {
+        id:9,
+        label: "Networking"
+    },
+    {
+        id:10,
+        label: "Just for laughs"
+    },
+    {
+        id:11,
+        label: "Suggestions"
+    },
+    {
+        id:12,
+        label: "Insight"
+    },
+    {
+        id:13,
+        label: "Industry"
+    },
+    {
+        id:14,
+        label: "Events"
+    },
+    {
+        id:15,
+        label: "Learning and development"
+    },
+    {
+        id:16,
+        label: "Other topics"
+    }
+]
+
+
+const oneWord = [
+    {
+        id:1,
+        label:"Strategic"
+    },
+    {
+        id:2,
+        label:"Insightful"
+    },
+    {
+        id:3,
+        label:"Determined"
+    },
+    {
+        id:4,
+        label:"Explorative"
+    },
+    {
+        id:5,
+        label:"Forward-looking"
+    },
+    {
+        id:6,
+        label:"Genuine"
+    },
+    {
+        id:7,
+        label:"Supportive"
+    },
+    {
+        id:8,
+        label:"Dedicated"
+    },
+    {
+        id:9,
+        label:"Caring"
+    },
+    {
+        id:10,
+        label:"Dependable"
+    },
+    {
+        id:11,
+        label:"Nurturing"
+    },
+    {
+        id:12,
+        label:"Easy-going"
+    },
+    {
+        id:13,
+        label:"Artistic"
+    },
+    {
+        id:14,
+        label:"Exciting"
+    },
+    {
+        id:15,
+        label:"Entertaining"
+    }
+    ]
+
+const isUpFor = [
+    {
+        id: 1,
+        label: "Coffee Chat"
+    },
+    {
+        id: 2,
+        label: "Zoom meetings"
+    },
+    {
+        id: 3,
+        label: "Conferences"
+    },
+    {
+        id: 4,
+        label: "Office parties"
+    },
+    {
+        id: 5,
+        label: "Collaborations"
+    },
+    {
+        id: 6,
+        label: "Afterword happy hour"
+    },
+    {
+        id: 7,
+        label: "Lunch meeting"
+    },
+    {
+        id: 8,
+        label: "Book club"
+    }
+]
+
+module.exports = { departments, topics, oneWord, isUpFor }
+
