0de5bfe142116c7a121b751d4a71febb5b64f9f5
lanomade <ganne.hannah@gmail.com>
2022-07-20 14:42:49 +0200

    edit to fetch posts from database

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,22 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
 import Feed from '../pages/feed'
 import { Outlet } from 'react-router-dom'
+import useFetch from '../utils/hooks/useFetch'
 
 export default function Home() {
     const [isSidebarShown, setIsSidebarShown] = useState(false)
-    const [isSignedIn, setIsSignedIn] = useState(true)
+    const [posts, setPosts] = useState([])
 
     function toggleSidebar() {
         setIsSidebarShown(prevState => !prevState)
     }
 
+    useEffect(() => {
+        const fetchOptions = {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`,
+            }
+        }
+
+        fetch('http://localhost:3000/api/posts', fetchOptions)
+            .then(res => res.json())
+            .then(data => setPosts(data))
+    }, [])
+
+    function handleFilter(department) {
+
+        const fetchOptions = {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`,
+            },
+            body: {"department" : department}
+        }
+
+        fetch('http://localhost:3000/api/posts/filter/department', fetchOptions)
+            .then(res => res.json())
+            .then(data => setPosts(data))
+    }
+
+    console.log(posts)
+
     return (
         <>
-            <Sidebar className="sidebar" isSidebarShown={isSidebarShown} />
+            <Sidebar className="sidebar" isSidebarShown={isSidebarShown} handleFilter={handleFilter} />
             <Header toggleSidebar={toggleSidebar} />
-            <Outlet />
+            <Outlet context={[posts, setPosts]}/>
         </>
     )
 }
