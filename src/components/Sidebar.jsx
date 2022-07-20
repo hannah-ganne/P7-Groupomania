c556a42b0e33627668e476e2f328fa6657da2c09
lanomade <ganne.hannah@gmail.com>
2022-07-20 14:39:52 +0200

    edit the component to render dynamically sidebar menus

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,12 +2,21 @@ import logo from '../assets/icon-left-font-monochrome-black.svg'
 import Button from './Button'
 import '../utils/style/Sidebar.css'
 import { Link } from 'react-router-dom'
+import { departments } from '../docs/list'
+import { useFetch } from '../utils/hooks/useFetch'
 
 export default function Sidebar(props) {
     let className
     props.isSidebarShown ? 
     className = `${props.className} show-sidebar`
-    : className = props.className
+        : className = props.className
+    
+    const menuEl = departments.map(item => {
+        return <div className="filter-dept" onClick={()=>props.handleFilter(item.label)}>
+                <i className={`${item.icon} fa-fw`}></i>
+                {item.label}
+                </div>
+    })
 
     return (
         <nav className={className}>
@@ -20,6 +29,7 @@ export default function Sidebar(props) {
             <ul className="sidebar--menu">
                 <li>
                     <Link to="/">All posts</Link>
+                    {menuEl}
                 </li>
             </ul>
             <div className="copyright">© Groupomania All rights reserved.</div>
