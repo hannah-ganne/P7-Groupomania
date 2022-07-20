f502a8fea0b44e308383dd30801721767afe04a2
lanomade <ganne.hannah@gmail.com>
2022-07-20 14:39:05 +0200

    edit the component to receive data dynamically in props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,26 +3,26 @@ import Tag from './Tag'
 import '../utils/style/Card.css'
 import { Link } from 'react-router-dom'
 
-export default function Card(props) {
+export default function Card({id, title, firstName, department, imageUrl, createdAt, description, topic, likesCount, commentsCount}) {
     return (
         <article>
-            <Avatar />
+            <Avatar imageUrl={imageUrl} />
             <div className="item--text">
-                <Link to="/post/1">
-                    <h3>Single Element Loaders: The Dots</h3>
+                <Link to={`/post/${id}`}>
+                    <h3>{title}</h3>
                     <p className="item--text--data">
-                        <span className="bold">Sarah</span> from <span className="bold">HR</span> started 1 day ago
+                        <span className="bold">{firstName}</span> from <span className="bold">{department}</span> started 1 day ago
                     </p>
-                    <p>We're looking at loaders in this series. More than that, we're breaking down some common loader patterns and how to re-create them with nothing more than a single div. So far, weve picked apart the...</p>
+                    <p>{description}</p>
                 </Link>
                 <div className="tags">
-                    <Tag name="HR" />
-                    <Tag name="News" />
+                    <Tag name={department} />
+                    <Tag name={topic} />
                 </div>
             </div>
             <div className="item--stats">
-                <p><span className="bold">6</span> comments</p>
-                <p><span className="bold">5</span> likes</p>
+                <p><span className="bold">{commentsCount}</span> {commentsCount <= 1 ? 'comment' : 'comments'}</p>
+                <p><span className="bold">{likesCount}</span> {likesCount <= 1 ? 'like' : 'likes'}</p>
             </div>
         </article>
     )
