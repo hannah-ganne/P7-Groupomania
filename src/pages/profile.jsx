59dda01f9f0f9ba5690b965ad21be5f0d63c605d
lanomade <ganne.hannah@gmail.com>
2022-07-21 12:47:30 +0200

    update useFetch paragraph

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -5,37 +5,61 @@ import Avatar from '../components/Avatar'
 import { Link } from 'react-router-dom'
 import useFetch from '../utils/hooks/useFetch'
 import { useState } from 'react'
+import { isUpFor } from '../docs/list'
 
 export default function Profile() {
-    const [myProfile, setMyProfile] = useState({})
+    // const [myProfile, setMyProfile] = useState({})
+    // let result = useFetch("GET", "http://localhost:3000/api/auth/viewProfile", setMyProfile);
 
-    useFetch("GET", "http://localhost:3000/api/auth/viewProfile", setMyProfile,)
+    const fetchOptions = {
+        method: "GET",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`,
+        }
+    }
+    const { data, loading, error } = useFetch('http://localhost:3000/api/auth/viewProfile', fetchOptions)
+    
+    if (error) {
+        console.log(error)
+    }
+
+    function checkVowel() {
+        const vowels = ['A', 'E', 'I', 'O', 'U']
+        return vowels.includes(data.oneWord.slice(0, 1)) ? 'an' : 'a'
+    } 
+
+    console.log(data)
     
     return (
-        <section className="profile">
+        <>
+            { loading && <div>loading...</div>}
+            { data &&  <section className="profile">
             <h2 className="profile-title">Profile</h2>
             <div className="profile-info">
                 <div className="profile-avatar-readonly">
                     <Avatar />
                 </div>    
                 <div>
-                    <p><span className="bold">Name: </span>{`${myProfile.firstName} ${myProfile.lastName}`}</p>
-                    <p><span className="bold">Department: </span>{myProfile.department}</p>
+                    <p><span className="bold">Name: </span>{`${data.firstName} ${data.lastName}`}</p>
+                    <p><span className="bold">Department: </span>{data.department}</p>
                 </div>
             </div>
             <div className="profile-detail">
                 <span className="bold uppercase">At work I'm expert in...</span>
-                <p>{myProfile.expertIn}</p>
+                <p>{data.expertIn}</p>
                 <span className="bold uppercase">Personally I'm interested in...</span>
-                <p>{myProfile.interestedIn}</p>
+                <p>{data.interestedIn}</p>
                 <span className="bold uppercase">Describe yourself in one word</span>
-                <p>I am a <span className='bold uppercase'>{myProfile.oneWord}</span> person</p>
+                <p>I am {checkVowel()} <span className='bold uppercase'>{data.oneWord}</span> person</p>
                 <span className="bold uppercase">I am up for...</span>
-                <p></p>
+                    {data.isUpFor.map(item => <p key={item.label}>{item.label}</p>)}
             </div>
             <Link to="/profile/edit">
                 <Button className="btn red" name="Modify profile" />
             </Link>
-        </section>
+        </section>}  
+        </>
     )
 }
