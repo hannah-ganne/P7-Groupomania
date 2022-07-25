2b0c890bcb2491e7ad7d720f2a9db434a2bb5788
lanomade <ganne.hannah@gmail.com>
2022-07-25 22:39:37 +0200

    add onClick attribute

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import '../utils/style/components.css'
 
 export default function Button(props) {
     return (
-        <button className={props.className} type={props.type}>
+        <button className={props.className} type={props.type} onClick={props.onClick}>
             {props.name}
         </button>
     )
