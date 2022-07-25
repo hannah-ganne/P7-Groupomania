45b345a020e2263b3e81087ec42edf692122d296
lanomade <ganne.hannah@gmail.com>
2022-07-25 22:49:00 +0200

    add sorting function

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,10 +1,36 @@
 import Card from '../components/Card'
 import '../utils/style/Feed.css'
 import { useOutletContext } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 export default function Feed() {
 
     const [posts, setPosts] = useOutletContext();
+    const [sortType, setSortType] = useState();
+
+    useEffect(() => {
+        const fetchOptions = {
+            method: "GET",
+            headers: {
+                "Accept": "application/json",
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`
+            },
+            body: sortType
+        }
+
+        fetch ('http://localhost:3000/api/posts', fetchOptions)
+        .then (res =>  {
+            if(res.ok) {
+                return res.json();
+            }
+            throw new Error("There's an error sending the data")
+        })
+        .then (data => {
+            setPosts(data)
+        })
+        .catch(err => console.log(err)); 
+    })
 
     const cardEl = posts.map(post => {
         return <Card
@@ -24,11 +50,11 @@ export default function Feed() {
 
     return (
     <section>
-        <select className="sort-items">
+        <select className="sort-items" value={sortType} onChange={(e) => setSortType(+e.target.value)}>
             <option value="">Sort</option>
-            <option value="latest">Latest</option>
-            <option value="oldest">Oldest</option>
-            <option value="mostPopular">Most Popular</option>
+            <option value="0">Latest</option>
+            <option value="1">Oldest</option>
+            <option value="2">Most Popular</option>
         </select>
         <div>
             {cardEl}
