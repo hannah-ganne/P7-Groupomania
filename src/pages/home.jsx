d911d552179f5bb1d1deaec162564c6de1326137
lanomade <ganne.hannah@gmail.com>
2022-07-27 23:54:29 +0200

    add responsive sidebar

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -3,15 +3,10 @@ import Sidebar from '../components/Sidebar'
 import Header from '../components/Header'
 import { Outlet } from 'react-router-dom'
 import useFetch from '../utils/hooks/useFetch'
+const drawerWidth = 300;
 
 export default function Home() {
-    const [isSidebarShown, setIsSidebarShown] = useState(false)
     const [posts, setPosts] = useState([])
-
-    function toggleSidebar() {
-        setIsSidebarShown(prevState => !prevState)
-    }
-
     useEffect(() => {
         const fetchOptions = {
             method: "GET",
@@ -22,10 +17,15 @@ export default function Home() {
             }
         }
 
-        fetch('http://localhost:3000/api/posts', fetchOptions)
+        fetch(`http://localhost:3000/api/posts/all/0`, fetchOptions)
             .then(res => res.json())
             .then(data => setPosts(data))
     }, [])
+
+    const [mobileOpen, setMobileOpen] = useState(false);
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
     function handleFilter(department) {
 
@@ -45,9 +45,62 @@ export default function Home() {
     
     return (
         <>
-            <Sidebar className="sidebar" isSidebarShown={isSidebarShown} handleFilter={handleFilter} />
-            <Header toggleSidebar={toggleSidebar} />
+            <Header handleDrawerToggle={handleDrawerToggle} />
+            <Sidebar mobileOpen={mobileOpen} handleDrawerToggle={handleDrawerToggle} />
             <Outlet context={[posts, setPosts]}/>
         </>
     )
 }
+// import { useEffect, useState } from 'react'
+// import Sidebar from '../components/Sidebar'
+// import Header from '../components/Header'
+// import { Outlet } from 'react-router-dom'
+// import useFetch from '../utils/hooks/useFetch'
+
+// export default function Home() {
+//     const [isSidebarShown, setIsSidebarShown] = useState(false)
+//     const [posts, setPosts] = useState([])
+
+//     function toggleSidebar() {
+//         setIsSidebarShown(prevState => !prevState)
+//     }
+
+//     useEffect(() => {
+//         const fetchOptions = {
+//             method: "GET",
+//             headers: {
+//                 "Accept": "application/json",
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`,
+//             }
+//         }
+
+//         fetch('http://localhost:3000/api/posts', fetchOptions)
+//             .then(res => res.json())
+//             .then(data => setPosts(data))
+//     }, [])
+
+//     function handleFilter(department) {
+
+//         const fetchOptions = {
+//             headers: {
+//                 "Accept": "application/json",
+//                 "Content-Type": "application/json",
+//                 "Authorization": `Bearer ${JSON.parse(sessionStorage.getItem("token"))}`,
+//             },
+//             body: {"department" : department}
+//         }
+
+//         fetch('http://localhost:3000/api/posts/filter/department', fetchOptions)
+//             .then(res => res.json())
+//             .then(data => setPosts(data))
+//     }
+    
+//     return (
+//         <>
+//             <Sidebar className="sidebar" isSidebarShown={isSidebarShown} handleFilter={handleFilter} />
+//             <Header toggleSidebar={toggleSidebar} />
+//             <Outlet context={[posts, setPosts]}/>
+//         </>
+//     )
+// }
