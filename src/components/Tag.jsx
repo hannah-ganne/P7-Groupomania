58db332e1379964ae5c6d826b8575e9b710a92df
lanomade <ganne.hannah@gmail.com>
2022-07-30 17:08:52 +0200

    add filtering function onClick

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -2,7 +2,14 @@ import '../utils/style/components.css'
 
 export default function Tag(props) {
     return (
-        <div className="tag">
+        <div
+            className="tag"
+            onClick={() => {
+            props.tagType === 'department'
+                ? props.setDepartment(props.name)
+                : props.setTopic(props.name)
+            }}
+        >
             {props.name}
         </div>
     )
