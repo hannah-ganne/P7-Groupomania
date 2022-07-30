6e398f6c8ff82d1590e9643b804206c7cfcc8110
lanomade <ganne.hannah@gmail.com>
2022-07-30 17:06:18 +0200

    add deleteComment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,16 +2,19 @@
 import Avatar from '@mui/material/Avatar'
 import deleteIcon from '../assets/delete.png'
 import DeleteIcon from '@mui/icons-material/Delete';
+import { IconButton } from '@mui/material';
 
 export default function Comment(props) {
     return (
-        <div className="comment">
+        <div className="comment" id={props.id}>
             <div>
             <Avatar src={props.imageUrl} />
             <span className='bold'>{props.userName}</span>
             <p>{props.comment}</p>
             </div>
-            <DeleteIcon className='delete-icon' fontSize='medium'/>
+            <IconButton className='delete-icon' onClick={(e) => props.deleteComment(e.target.closest('div').id)}>
+                <DeleteIcon fontSize='medium' />
+            </IconButton>
         </div>
     )
 }
