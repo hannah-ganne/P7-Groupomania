c8bb7878cc0d6726e7284c898a7ef7f3127b1c0d
lanomade <ganne.hannah@gmail.com>
2022-08-07 15:21:28 +0200

    delete unused code

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -3,7 +3,7 @@ import Avatar from '@mui/material/Avatar'
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
 import '../utils/style/components.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import DropdownMenu from '../components/DropdownMenu'
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import CustomModal from '../components/CustomModal'
@@ -12,7 +12,7 @@ import { useOutletContext } from 'react-router-dom';
 export default function Admin() {
 
     const { data, error, loading } = useFetch('GET', 'http://localhost:3000/api/auth/users')
-    const [setUserId] = useOutletContext()
+    const [posts, setPosts, userId, setUserId] = useOutletContext()
 
     function deleteUser(userId) {
         const fetchOptions = {
@@ -37,7 +37,6 @@ export default function Admin() {
         .catch(err => console.log(err)); 
     }
 
-
     function UserInfo(props) {
 
         const [modalOpen, setModalOpen] = useState(false);
@@ -45,36 +44,11 @@ export default function Admin() {
 
         return (
         <div className="user-info" id={props.id}>
-            <DropdownMenu
-                button={(<Avatar src={props.imageUrl} />)}
-                menuIcon1={(<AccountCircleIcon />)}
-                menuName1='View profile'
-                menuOnClick1={handleOpen}
-                menuLink1='#'
-                menuIcon2={(<AccountCircleIcon />)}
-                menuName2='View posts'
-                    menuOnClick2={() => {
-                        setUserId(props.id)
-                    }}    
-                menuLink2={'#'}
-            />
-            <CustomModal
-                modalOpen={modalOpen}
-                onModalChange={(open) => setModalOpen(open)}
-                imageUrl={props.imageUrl}
-                firstName={props.firstName}
-                lastName={props.lastName}
-                department={props.department}
-                expertIn={props.expertIn}
-                interestedIn={props.interestedIn}
-                oneWord={props.oneWord}
-                isUpFor={props.isUpFor
-                        .filter(item => item.checked)}
-            />
+            <Avatar src={props.imageUrl} />
             <p>
                 <span className='bold'>{`${props.firstName} ${props.lastName}`}</span>
-                from <span className='bold'>{props.department}</span>
-                joined on <span className='bold'>{new Date(props.createdAt).getDate() + '/' + new Date(props.createdAt).getMonth() + '/' + new Date(props.createdAt).getFullYear()}</span>        
+                from {props.department ? <span className='bold'>{props.department}</span> : 'undefined '}
+                joined on <span className='bold'>{new Date(props.createdAt).getDate() + '/' + (new Date(props.createdAt).getMonth()+1) + '/' + new Date(props.createdAt).getFullYear()}</span>        
             </p>        
             <IconButton className='delete-icon' onClick={(e) => deleteUser(e.target.closest('div').id)}>
                 <DeleteIcon fontSize='medium' />
@@ -94,18 +68,18 @@ export default function Admin() {
             <h2 className='profile-title'>Admin Dashboard</h2>
                 {data.map(user => {
                     return <UserInfo
-                        key={user.id}    
-                        id={user.id}
-                        imageUrl={user.imageUrl}
-                        firstName={user.firstName}
-                        lastName={user.lastName}
-                        department={user.department}
-                        expertIn={user.expertIn}
-                        interestedIn={user.interestedIn}
-                        oneWord={user.oneWord}
-                        createdAt={user.createdAt}
-                        isUpFor={user.isUpFor}
-                    />
+                            key={user.id}    
+                            id={user.id}
+                            imageUrl={user.imageUrl}
+                            firstName={user.firstName}
+                            lastName={user.lastName}
+                            department={user.department}
+                            expertIn={user.expertIn}
+                            interestedIn={user.interestedIn}
+                            oneWord={user.oneWord}
+                            createdAt={user.createdAt}
+                            isUpFor={user.isUpFor}
+                            />
             })}
     </section>
     }
