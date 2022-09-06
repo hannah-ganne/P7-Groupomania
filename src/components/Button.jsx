ed884a2334483ce12f3d8dce77e7dd5810161fa4
lanomade <48105337+lanomade@users.noreply.github.com>
2022-09-06 12:14:34 +0200

    Delete src directory

--- src/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import '../utils/style/components.css'
-
-export default function Button(props) {
-    return (
-        <button className={props.className} type={props.type} onClick={props.onClick}>
-            {props.name}
-        </button>
-    )
-}
